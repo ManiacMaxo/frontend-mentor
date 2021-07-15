7b15f4b39d4966a1180a8172cea629dbb4998bc1
Victor Gorchilov <victor@gorchilov.com>
2021-07-15 18:30:22 +0300

    [up] global button class

--- calculator/src/components/CopyToClip.svelte
+++ calculator/src/components/CopyToClip.svelte
@@ -30,62 +30,66 @@
     }
 </script>
 
-<div class="hidden">
-    <label for="copy">Copy to Clipboard</label>
-    <textarea name="copy" id="copy" bind:this={areaDom}>{valueCopy}</textarea>
-</div>
-<button class="clipboard" on:click={copy} aria-label="copy to clipboard">
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
+<div class="clipboard">
+    <div class="hidden">
+        <label for="copy">clipboard</label>
+        <textarea name="copy" id="copy" bind:this={areaDom}
+            >{valueCopy}</textarea
+        >
+    </div>
+    <button
+        class="button clipboard-button"
+        on:click={copy}
+        aria-label="copy to clipboard"
     >
-        <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-        />
-    </svg>
-</button>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+            />
+        </svg>
+    </button>
+</div>
 
 <style lang="scss">
     .clipboard {
-        --offset: 4px;
-
-        color: var(--colors-text-key-secondary, var(--colors-text));
-        background-color: var(--colors-key-accent);
-        box-shadow: 0px var(--offset) 0px 0px var(--colors-key-accent-shadow);
-
         position: absolute;
         top: 50%;
-        transform: translateY(calc(-50% - var(--offset)));
+        transform: translateY(-50%);
+    }
+
+    .clipboard-button {
+        --shadow-color: var(--colors-key-accent-shadow);
+        --text-color: var(
+            --colors-text-key-secondary,
+            var(--colors-text-key-mod)
+        );
+        --bg-color: var(--colors-key-accent);
 
         padding: 5px;
-        border-radius: 10%;
-
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        cursor: pointer;
+        height: calc(1em + 2 * 5px);
 
         svg {
             width: 1em;
             height: 1em;
         }
-
-        &:active {
-            --offset: 1px;
-        }
     }
 
     .hidden {
         opacity: 0;
-        position: absolute;
 
+        &,
         * {
+            position: absolute;
+            top: 0;
+
             width: 0;
             height: 0;
         }
--- calculator/src/components/Keypad.svelte
+++ calculator/src/components/Keypad.svelte
@@ -34,70 +34,54 @@
 
 <svelte:body on:keydown={keyPress} />
 
-<button class="key" on:click={onClick}>7</button>
-<button class="key" on:click={onClick}>8</button>
-<button class="key" on:click={onClick}>9</button>
-<button class="key mod" on:click={del}>del</button>
-<button class="key" on:click={onClick}>4</button>
-<button class="key" on:click={onClick}>5</button>
-<button class="key" on:click={onClick}>6</button>
-<button class="key" on:click={onClick}>+</button>
-<button class="key" on:click={onClick}>1</button>
-<button class="key" on:click={onClick}>2</button>
-<button class="key" on:click={onClick}>3</button>
-<button class="key" on:click={onClick}>-</button>
-<button class="key" on:click={onClick}>.</button>
-<button class="key" on:click={onClick}>0</button>
-<button class="key" on:click={onClick}>/</button>
-<button class="key" on:click={onClick}>x</button>
-<button class="key wide mod" on:click={reset}>reset</button>
-<button class="key wide accent" on:click={solve}>=</button>
+<button class="button key" on:click={onClick}>7</button>
+<button class="button key" on:click={onClick}>8</button>
+<button class="button key" on:click={onClick}>9</button>
+<button class="button key mod" on:click={del}>del</button>
+<button class="button key" on:click={onClick}>4</button>
+<button class="button key" on:click={onClick}>5</button>
+<button class="button key" on:click={onClick}>6</button>
+<button class="button key" on:click={onClick}>+</button>
+<button class="button key" on:click={onClick}>1</button>
+<button class="button key" on:click={onClick}>2</button>
+<button class="button key" on:click={onClick}>3</button>
+<button class="button key" on:click={onClick}>-</button>
+<button class="button key" on:click={onClick}>.</button>
+<button class="button key" on:click={onClick}>0</button>
+<button class="button key" on:click={onClick}>/</button>
+<button class="button key" on:click={onClick}>x</button>
+<button class="button key wide mod" on:click={reset}>reset</button>
+<button class="button key wide accent" on:click={solve}>=</button>
 
 <style lang="scss">
     .key {
         --shadow-color: var(--colors-key-shadow);
-        --offset: 5px;
-
-        background-color: var(--colors-key);
-        box-shadow: 0px var(--offset) 0px 0px var(--shadow-color);
-        color: var(--colors-text-key);
+        --text-color: var(--colors-text-key);
+        --bg-color: var(--colors-key);
 
         font-size: 0.75em;
 
         padding: 0.75em 0.75em 0.5em;
-        border-radius: 10px;
-
-        cursor: pointer;
 
         &.mod {
             --shadow-color: var(--colors-key-mod-shadow);
-
-            background-color: var(--colors-key-mod);
-            color: var(--colors-text-key-mod);
+            --text-color: var(--colors-text-key-mod);
+            --bg-color: var(--colors-key-mod);
 
             text-transform: uppercase;
         }
 
         &.accent {
             --shadow-color: var(--colors-key-accent-shadow);
-
-            background-color: var(--colors-key-accent);
-            color: var(--colors-text-key-secondary, var(--colors-text));
+            --text-color: var(
+                --colors-text-key-secondary,
+                var(--colors-text-key-mod)
+            );
+            --bg-color: var(--colors-key-accent);
         }
 
         &.wide {
             grid-column-end: span 2;
-        }
-
-        &:hover,
-        &:focus {
-            outline: 0;
-            filter: brightness(0.8);
-        }
-
-        &:active {
-            --offset: 1px;
-            transform: translateY(4px);
         }
     }
 </style>
