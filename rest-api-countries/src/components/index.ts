17c38cf576a4261f166f0ca7581b755afd6dcd19
Victor Gorchilov <victor@gorchilov.com>
2023-10-29 15:46:50 +0200

    delete junk

--- rest-api-countries/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Card } from './Card.vue'
-export { default as GlobalHeader } from './GlobalHeader.vue'
--- svelte-rest-api-countries/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Card } from './Card.svelte'
-export { default as Filter } from './Filter.svelte'
-export { default as Header } from './Header.svelte'
-export { default as Search } from './Search.svelte'
